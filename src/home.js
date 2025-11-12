fc5081ec960489af8006b92f617f870e58069b19
favibe <ibefavour85@gmail.com>
2025-11-12 03:42:11 +0100

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,4 +1,14 @@
-function loadHomePage() {
+ export function loadHomePage() {
     const content = document.querySelector(".content");
-    
+
+   const homeDiv = document.createElement("div");
+   const heading = document.createElement("h1")
+   const para = document.createElement("p");
+
+   headline.textContent = "Welcome to Elle's Kitchen!";
+   description.textContent = "The best meals, freshly cooked with love 💖";
+
+   homeDiv.appendChild(heading);
+   homeDiv.appendChild(para);
+   content.appendChild(homeDiv);
 }
